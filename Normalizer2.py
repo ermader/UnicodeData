2babcd21a50a533fa249fc9422e46995dc79cebe
Eric Mader <emader@gmx.us>
2020-06-04 10:46:39 -1000

    Get characters to test from a list so all test test the same characters.

--- Normalizer2.py
+++ Normalizer2.py
@@ -328,45 +328,24 @@ def test():
     trie = Normalizer2.createFromHardCodedData()
     nfkcTrie = Normalizer2.createFromFileData("nfkc")
 
+    charList = [0x0041, 0x00A0, 0x00A8, 0x00BE, 0x00C0, 0x1EA6, 0x3307, 0x6569, 0xCA8D, 0xFA6C]
+
     print("NFC:")
-    print(f"getDecomposition('A') is {decompToCharList(trie, ord('A'))}")
-    print(f"getDecomposition('{chr(0x00A0)}') is {decompToCharList(trie, 0x00A0)}")
-    print(f"getDecomposition('{chr(0x00A8)}') is {decompToCharList(trie, 0x00A8)}")
-    print(f"getDecomposition('{chr(0x00BE)}') is {decompToCharList(trie, 0x00BE)}")
-    print(f"getDecomposition('{chr(0x00C0)}') is {decompToCharList(trie, 0x00C0)}")
-    print(f"getDecomposition('{chr(0x1EA6)}') is {decompToCharList(trie, 0x1EA6)}")
-    print(f"getDecomposition('{chr(0x3307)}') is {decompToCharList(trie, 0x3307)}")
-    print(f"getDecomposition('{chr(0xCA8D)}') is {decompToCharList(trie, 0xCA8D)}")
-    print(f"getDecomposition('{chr(0xFA6C)}') is {decompToCharList(trie, 0xFA6C)}")
+    for ch in charList:
+        print(f"getDecomposition('{chr(ch)}') is {decompToCharList(trie, ch)}")
     print()
 
-    print(f"getRawDecomposition('A') is {rawDecompToCharList(trie, ord('A'))}")
-    print(f"getRawDecomposition('{chr(0x00A0)}') is {rawDecompToCharList(trie, 0x00A0)}")
-    print(f"getRawDecomposition('{chr(0x00A8)}') is {rawDecompToCharList(trie, 0x00A8)}")
-    print(f"getRawDecomposition('{chr(0x00BE)}') is {rawDecompToCharList(trie, 0x00BE)}")
-    print(f"getRawDecomposition('{chr(0x00C0)}') is {rawDecompToCharList(trie, 0x00C0)}")
-    print(f"getRawDecomposition('{chr(0x1EA6)}') is {rawDecompToCharList(trie, 0x1EA6)}")
-    print(f"getRawDecomposition('{chr(0x3307)}') is {rawDecompToCharList(trie, 0x3307)}")
-    print(f"getRawDecomposition('{chr(0xCA8D)}') is {rawDecompToCharList(trie, 0xCA8D)}")
-    print(f"getRawDecomposition('{chr(0x6595)}') is {rawDecompToCharList(trie, 0x6595)}")
-    print(f"getRawDecomposition('{chr(0xFA6C)}') is {rawDecompToCharList(trie, 0xFA6C)}")
+    for ch in charList:
+        print(f"getRawDecomposition('{chr(ch)}') is {rawDecompToCharList(trie, ch)}")
     print()
 
     print("NFKC:")
-    print(f"getDecomposition('{chr(0x00A0)}') is {decompToCharList(nfkcTrie, 0x00A0)}")
-    print(f"getDecomposition('{chr(0x00A8)}') is {decompToCharList(nfkcTrie, 0x00A8)}")
-    print(f"getDecomposition('{chr(0x00BE)}') is {decompToCharList(nfkcTrie, 0x00BE)}")
-    print(f"getDecomposition('{chr(0x3307)}') is {decompToCharList(nfkcTrie, 0x3307)}")
-    print(f"getDecomposition('{chr(0xCA8D)}') is {decompToCharList(nfkcTrie, 0xCA8D)}")
-    print(f"getDecomposition('{chr(0xFA6C)}') is {decompToCharList(nfkcTrie, 0xFA6C)}")
+    for ch in charList:
+        print(f"getDecomposition('{chr(ch)}') is {decompToCharList(nfkcTrie, ch)}")
     print()
 
-    print(f"getRawDecomposition('{chr(0x00A0)}') is {rawDecompToCharList(nfkcTrie, 0x00A0)}")
-    print(f"getRawDecomposition('{chr(0x00A8)}') is {rawDecompToCharList(nfkcTrie, 0x00A8)}")
-    print(f"getRawDecomposition('{chr(0x00BE)}') is {rawDecompToCharList(nfkcTrie, 0x00BE)}")
-    print(f"getRawDecomposition('{chr(0x3307)}') is {rawDecompToCharList(nfkcTrie, 0x3307)}")
-    print(f"getRawDecomposition('{chr(0xCA8D)}') is {rawDecompToCharList(nfkcTrie, 0xCA8D)}")
-    print(f"getRawDecomposition('{chr(0xFA6C)}') is {rawDecompToCharList(nfkcTrie, 0xFA6C)}")
+    for ch in charList:
+        print(f"getRawDecomposition('{chr(ch)}') is {rawDecompToCharList(nfkcTrie, ch)}")
 
 
 if __name__ == "__main__":
