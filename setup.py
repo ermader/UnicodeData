d559bc87fdabe6a07a26b9432fdf5c4858bf682f
Eric Mader <emader@gmx.us>
2022-04-15 12:07:35 -1000

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="UnicodeData",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     package_data={
         "":  ["Data/*.dat"],
